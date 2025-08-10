1507195cd19d1432b925430efba69cf9065f5a38
Selçuk Karakayalı <skarakayali@gmail.com>
2025-08-10 16:46:53 +0100

    next

---
+++ zsh/custom/sdkman.zsh
@@ -0,0 +1,5 @@
+# Initialize SDKMAN
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+export SDKMAN_AUTO_ENV=true
+export SDKMAN_AUTO_ANSWER=true
